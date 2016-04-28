001568a3bdb4b7458dbdc2cb06df9c1e08841731
Ben Brown <ben@ben-brown.co>
2016-04-28 16:10:25 +0100

    Basic js inheritance example

---
+++ basic-example.js
@@ -0,0 +1,44 @@
+// Object Composition using Factory Functions (no 'this' used)
+
+const engine = (state) => ({
+  start: () => {
+    console.log('Engine is running, using 1 litre of ' + state.fuelType + ' per mile.');
+  },
+  stop: () => {
+    console.log('Engine has stopped. 10 litres of ' + state.fuelType + ' left.')
+  }
+});
+
+const motorbike = (fuelType) => {
+  let state = {
+    fuelType
+  }
+
+  return Object.assign({},
+    engine(state)
+  )
+}
+
+const ducati = motorbike('petrol');
+ducati.start();
+ducati.stop();
+
+// Prototype Inheritance without 'New'
+
+const car = {
+  init: function(manufacturer, model) {
+    this.manufacturer = manufacturer;
+    this.model = model;
+  },
+  whatCar: function() {
+    console.log('This car is a ' + this.manufacturer + ' ' + this.model);
+  }
+}
+
+const bmw = Object.create(car);
+bmw.init('BMW', '135i');
+bmw.whatCar();
+
+const porsche = Object.create(car);
+porsche.init('Porsche', 'Cayman S');
+porsche.whatCar();
